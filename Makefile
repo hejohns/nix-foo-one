84a99b108dfa0bf0f9b4ddae45caf89bf29cfa6e
hejohns <hejohns@umich.edu>
2022-08-13 17:36:45 -0400

    bump

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
-one: main.o one.o
+${name}: main.o one.o
 	gcc main.o one.o -o $@
-	mkdir -p $(out)/bin
-	mv $@ $(out)/bin/
 main.o: main.c one.h
 	gcc main.c -c
 one.o: one.c one.h
--- flake.nix
+++ flake.nix
@@ -7,27 +7,58 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
         let pkgs = import nixpkgs {
-          inherit system;
+          inherit system; # why does import nixpkgs eval to a function expecting a set w/ attribute system?
         }; in
         rec {
           packages = flake-utils.lib.flattenTree {
-            one = pkgs.stdenv.mkDerivation {
-              name = "one";
-              src = self;
+            one = pkgs.stdenv.mkDerivation rec {
+              # the attributes will all be set env variables
+              # (see src)
+              #name = "one"; # required by stdenv. But it's preferred to set pname and version instead
+              pname = "one";
+              version = "0.0";
+              src = self; # literally sets $src to tmp dir that is a clone of this dir (check w/ nix develop)
+              strictDeps = true;
               buildInputs = [
                 nixpkgs.legacyPackages.${system}.gcc
                 nixpkgs.legacyPackages.${system}.clang
+                nixpkgs.legacyPackages.${system}.SDL
               ];
+              postUnpack = ''
+                echo 'postUnpack ed!'
+                '';
+              postPatch = ''
+                echo 'postPatch ed!'
+                '';
               configurePhase = ''
-                make
-              '';
+                pwd
+                ls
+                env
+                echo HERE
+                gcc -v
+                echo THERE
+                '';
               buildPhase = ''
                 make
-              '';
+                '';
+              postBuild = ''
+                echo "this doesn't run twice right?"
+                '';
+              doCheck = true;
+              checkPhase = ''
+                echo 'checking...?'
+                stat $name
+                '';
               installPhase = ''
-                make
-              '';
+                mkdir -p $out/bin
+                cp $name $out/bin
+                '';
+              postInstall = ''
+                ls $out
+                '';
+              seperateDebugInfo = true;
               program = "/bin/gcc";
+              # TODO:
               outputs = [ "out" ];
             };
           };
@@ -35,6 +66,7 @@
           apps = {
             one = flake-utils.lib.mkApp {
               drv = packages.one;
+              name = packages.one.name;
             };
           };
           defaultApp = apps.one;
